a602d8c21d9a5c3065dc70dd05799a7f5a66f183
Vedadnya Jadhav <vedadnya@gmail.com>
2020-06-05 20:41:06 -0400

    final

--- app.js
+++ app.js
@@ -9,20 +9,10 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 
-// // View engine setup
-// app.engine('handlebars', exphbs());
-// app.set('view engine', 'handlebars');
-
 // Static folder
 app.use('/public', express.static(path.join(__dirname, 'public')));
-
-// Body Parser Middleware
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-// app.use(express.multipart());
 
 
 app.get('/', (req, res) => {
@@ -40,33 +30,31 @@ app.post('/send', (req, res) => {
 
   // setup email data with unicode symbols
   let mailOptions = {
-      // from: '"Venugopal Balaji" <venugopalportfolio@gmail.com>', // sender address
-      // from:"venugopalportfolio@gmail.com",
-      from:"vedadnya.portfolio@gmail.com",
-      to: 'vedadnya@gmail.com', // list of receivers
+      from:process.env.EMAIL,
+      to: process.env.TO, // list of receivers
       subject: req.body.subject, // Subject line
       text: req.body.message, // plain text body
       html: 'Message from: ' + req.body.name + '<br></br> Email: ' +  req.body.email + '<br></br> Message: ' + req.body.message // html body
   };
 
  // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port:465,
-      secure:true,
-      // secure: false, // true for 465, false for other ports
-      auth: {
-      type: 'oauth2',
-      user: process.env.USER, // generated ethereal user
-      pass: process.env.PASS,  // generated ethereal password
-      clientId: process.env.client_id,
-      clientSecret: process.env.client_secret,
-      refreshToken: process.env.refreshToken,
-    },
-    tls:{
-    rejectUnauthorized:false
-    }
-    });
+ let transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port:465,
+  secure:true,
+  // secure: false, // true for 465, false for other ports
+  auth: {
+  type: 'oauth2',
+  user: process.env.EMAIL, // generated ethereal user
+  pass: process.env.PASSWORD,  // generated ethereal password
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  refreshToken: process.env.REFRESH_TOKEN,
+},
+tls:{
+rejectUnauthorized:false
+}
+});
   // send mail with defined transport object
   transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
